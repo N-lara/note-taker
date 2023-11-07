5145344d0878bc10cef0812f99b553632cc0dff8
N-lara <n_lara01@yahoo.com>
2023-11-07 10:44:40 -0600

    fixed server issues and set paths for directory

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const path = require('path');
-
 const api = require('./routes/index')
-// Helper functions for reading and writing to the JSON file
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const PORT = 3001;
 const app = express();
@@ -23,37 +20,9 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for notes page
-app.get('/feedback', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
-
-// TODO move the /api/notes routes to their own file???????
-
-// GET Route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request received for notes`);
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
-
-// POST Route for a new UX/UI tip
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
-  const { noteTitle, noteBody } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      noteTitle,
-      noteBody,
-      tip_id: uuid(),
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`note added successfully`);
-  } else {
-    res.error('Error in adding note');
-  }
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
