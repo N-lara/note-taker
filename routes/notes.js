0e83519e1193a7943ffd8a7c75c86a0e2eb43ffb
N-lara <n_lara01@yahoo.com>
2023-11-08 00:07:38 -0600

    added delete functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
 const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
 //GET route for retrieving notes
 notes.get('/', (req,res)=>{
     console.info(`${req.method} request recieved for notes`)
@@ -30,20 +31,21 @@ notes.post('/', (req,res)=>{
 
 //future functionality for delete
 
-// //checks and deletes array
-//   function Deletednote(id){
-//     let notesArray;
-//       function check(arr){return arr.id !== id}
-//       readFromFile('./db/db.json').then((data) => {
-//         notesArray = data.filter(check);   
-//       });
-//       writeToFile('../../db/db.json', notesArray);
-//       return notesArray;
-//   }
+//checks and deletes array
+  function deletednote(id){
+    const notesArray = require('../db/db.json')
+    let array = notesArray;
+    for(let i=0; i<array.length; i++){
+      if(array[i].id == id){
+        array.splice(i,1);
+      }
+    }
+    console.log(array);
+    writeToFile('./db/db.json', array);
+  }
     
-//     notes.delete('/:id', (req, res)=>{
-//       const note = Deletednote(req.params.id);
-//       res.json(note);
-//     })
+    notes.delete('/:id', (req, res)=>{
+      deletednote(req.params.id);
+    })
 
 module.exports = notes;
